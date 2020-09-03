6aa0615b2fef77ec6c1c1e22aef15b8f068dce4f
Grzegorz Ciesielski <tao24.k@gmail.com>
2020-09-03 13:44:19 +0200

    feat: card component, rewritten styling

---
+++ src/app/shared/components/card/card.component.ts
@@ -0,0 +1,35 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.scss'],
+})
+export class CardComponent implements OnInit {
+  @Input() color: string;
+  colorClass;
+
+  ngOnInit() {
+    console.log(this.color);
+    this.colorClass = {
+      'primary-dark': this.color === 'primary-dark' || this.color === 'dark',
+      'primary-medium-dark': this.color === 'primary-medium-dark' || this.color === 'medium-dark',
+      primary: this.color === 'primary' || this.color === 'default',
+      'primary-medium-light': this.color === 'primary-medium-light' || this.color === 'medium-light',
+      'primary-light': this.color === 'primary-light' || this.color === 'light',
+      'primary-very-light': this.color === 'primary-very-light' || this.color === 'very-light',
+      'secondary-dark': this.color === 'secondary-dark',
+      'secondary-medium-dark': this.color === 'secondary-medium-dark',
+      secondary: this.color === 'secondary',
+      'secondary-medium-light': this.color === 'secondary-medium-light',
+      'secondary-light': this.color === 'secondary-light',
+      'secondary-very-light': this.color === 'secondary-very-light',
+      'tertiary-dark': this.color === 'tertiary-dark',
+      'tertiary-medium-dark': this.color === 'tertiary-medium-dark',
+      tertiary: this.color === 'tertiary',
+      'tertiary-medium-light': this.color === 'tertiary-medium-light',
+      'tertiary-light': this.color === 'tertiary-light',
+      'tertiary-very-light': this.color === 'tertiary-very-light',
+    };
+  }
+}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,14 +3,15 @@ import { NgModule } from '@angular/core';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { ChartsModule } from 'ng2-charts';
 import { MaterialModule } from '../material.module';
+import { CardComponent } from './components/card/card.component';
 import { IconComponent } from './components/icon/icon.component';
 import { AmountPipe } from './pipes/amount.pipe';
 import { CategoryPipe } from './pipes/category.pipe';
 import { DatePipe } from './pipes/date.pipe';
 
 @NgModule({
-  declarations: [IconComponent, CategoryPipe, DatePipe, AmountPipe],
+  declarations: [IconComponent, CardComponent, CategoryPipe, DatePipe, AmountPipe],
   imports: [CommonModule, ChartsModule, FlexLayoutModule, MaterialModule],
-  exports: [FlexLayoutModule, ChartsModule, MaterialModule, IconComponent, CategoryPipe, DatePipe, AmountPipe],
+  exports: [FlexLayoutModule, ChartsModule, MaterialModule, IconComponent, CardComponent, CategoryPipe, DatePipe, AmountPipe],
 })
 export class SharedModule {}
